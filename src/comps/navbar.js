7ef61b283821dc753e2908a07c8522ec62e8e241
Zeina <zeinaalshal@gmail.com>
2024-07-31 12:36:51 +0300

    zee52

--- src/comps/navbar.js
+++ src/comps/navbar.js
@@ -8,32 +8,38 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setMenuActive(!menuActive);
+    var menu = document.querySelector(".navbar ul.menu");
+    menu.classList.toggle("active");
   };
-
   return (
     <nav>
-
-    <div class="nav-container">
-    <div class="container">
-      <div class="navbar">
-        <a href="#" class="logo">
-          <img src="icon12.png" width="35px" alt="Logo" />
-          <span class="logo-primary">N</span>issan
-          <span class="logo-primary">V</span>ehicles
-        </a>
-        <div class="mobile-menu">
-          <div class="burger-menu menu-toggle" onclick="toggleMenu()">
-            <i class="fas fa-bars"></i>
+      <div class="nav-container">
+        <div class="container">
+          <div class="navbar">
+            <a href="#" class="logo">
+              <img src="icon12.png" width="35px" alt="Logo" />
+              <span class="logo-primary">N</span>issan
+              <span class="logo-primary">V</span>ehicles
+            </a>
+            <div class="mobile-menu">
+              <div class="burger-menu menu-toggle" onClick={toggleMenu}>
+                <i class="fas fa-bars"></i>
+              </div>
+              <ul class="menu">
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#details">Details</a>
+                </li>
+                <li>
+                  <a href="#login">Login</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <ul class="menu">
-            <li><a href="#about">About</a></li>
-            <li><a href="#details">Details</a></li>
-            <li><a href="#login">Login</a></li>
-          </ul>
         </div>
       </div>
-    </div>
-  </div>
     </nav>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import "../app/globals.css";
 import { useEffect } from "react";
-import Layout from "@/comps/layout";
 
 export default function Home() {
   useEffect(() => {
@@ -30,12 +29,7 @@ export default function Home() {
         });
       });
     };
-  }, []); 
-
-  function toggleMenu() {
-    var menu = document.querySelector(".navbar ul.menu");
-    menu.classList.toggle("active");
-  }
+  }, []);
 
   return (
     <>
@@ -44,92 +38,91 @@ export default function Home() {
         <meta name="description" content="Nissan Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
       <div class="imagecontainer" id="home">
-      <img src="nissan-kicks.webp" width="100%" alt="Nissan Kicks" />
-      <div class="container">
-        <p class="caption">By Your Side...</p>
+        <img src="nissan-kicks.webp" width="100%" alt="Nissan Kicks" />
+        <div class="container">
+          <p class="caption">By Your Side...</p>
+        </div>
       </div>
-    </div>
 
-    <div id="about" class="section">
-      <div class="container">
-        <h2 class="section-title">About Nissan</h2>
-        <div class="section">
-          <div class="container-about">
-            <div class="content-section">
-              <div class="content">
-                <p>
-                  Nissan has a long history of innovation and excellence in the
-                  automotive industry. Founded in 1933, Nissan has been at the
-                  forefront of developing groundbreaking vehicles that combine
-                  performance, technology, and design.
-                </p>
-                <div class="button-readmore">
-                  <button class="button-alone" type="submit">Read more</button>
+      <div id="about" class="section">
+        <div class="container">
+          <h2 class="section-title">About Nissan</h2>
+          <div class="section">
+            <div class="container-about">
+              <div class="content-section">
+                <div class="content">
+                  <p>
+                    Nissan has a long history of innovation and excellence in
+                    the automotive industry. Founded in 1933, Nissan has been at
+                    the forefront of developing groundbreaking vehicles that
+                    combine performance, technology, and design.
+                  </p>
+                  <div class="button-readmore">
+                    <button class="button-alone" type="submit">
+                      Read more
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="image-section">
-              <img src="car10.webp" width="100%" alt="Nissan" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="details" class="section">
-      <div class="container">
-        <h2 class="section-title">DETAILS!</h2>
-        <div class="card-container">
-          <div class="card">
-            <div class="classpic">
-              <img src="nissan-kick.webp" alt="Image 1" />
-            </div>
-            <div class="card-content">
-              <h3>Nissan GT-R</h3>
-              <p>PRICE: $213,775</p>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="classpic">
-              <img src="nissan2.webp" alt="Image 2" />
-            </div>
-            <div class="card-content">
-              <h3>Nissan Sentra</h3>
-              <p>PRICE: $113,540</p>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="classpic">
-              <img src="nissan3.webp" alt="Image 3" />
-            </div>
-            <div class="card-content">
-              <h3>Nissan Rogue</h3>
-              <p>PRICE: $24,300</p>
+              <div class="image-section">
+                <img src="car10.webp" width="100%" alt="Nissan" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div id="login" class="section">
-      <h2 class="section-title">LOGIN!</h2>
+
+      <div id="details" class="section">
+        <div class="container">
+          <h2 class="section-title">DETAILS!</h2>
+          <div class="card-container">
+            <div class="card">
+              <div class="classpic">
+                <img src="nissan-kick.webp" alt="Image 1" />
+              </div>
+              <div class="card-content">
+                <h3>Nissan GT-R</h3>
+                <p>PRICE: $213,775</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="classpic">
+                <img src="nissan2.webp" alt="Image 2" />
+              </div>
+              <div class="card-content">
+                <h3>Nissan Sentra</h3>
+                <p>PRICE: $113,540</p>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="classpic">
+                <img src="nissan3.webp" alt="Image 3" />
+              </div>
+              <div class="card-content">
+                <h3>Nissan Rogue</h3>
+                <p>PRICE: $24,300</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div id="login" class="section">
-        <form class="login-form" action="#">
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-          <label for="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-          <label for="birthdate">Birthdate:</label>
-          <input type="date" id="birthdate" name="birthdate" required />
-          <button type="submit">Login</button>
-        </form>
+        <h2 class="section-title">LOGIN!</h2>
+        <div id="login" class="section">
+          <form class="login-form" action="#">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+            <label for="birthdate">Birthdate:</label>
+            <input type="date" id="birthdate" name="birthdate" required />
+            <button type="submit">Login</button>
+          </form>
+        </div>
       </div>
-    </div>
-    
-      </Layout>
     </>
   );
 }
